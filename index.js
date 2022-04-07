a3df4651dc168d98b46772bd9b70367eb9842270
Andre Wright <andremwright@outlook.com>
2022-04-07 16:03:47 -0400

    Add function that generates random computer hand

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+let computerResult;
+
+function computerPlay() {
+    computerResult = Math.floor((Math.random() * 3) + 1);
+    if (computerResult == 1) {
+        computerResult = "Rock";
+    } else if (computerResult == 2) {
+        computerResult = "Paper";
+    } else {
+        computerResult = "Scissors";
+    }
+}
+
+computerPlay();
+
+console.log(computerResult);
