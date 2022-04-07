a16b29adfbbdf88dd4f6262c0140dbed85ec0e5e
Andre Wright <andremwright@outlook.com>
2022-04-07 17:15:52 -0400

    Add function that declares the winner

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ let computerSelection;
 function computerPlay() {
     computerSelection = Math.floor((Math.random() * 3) + 1);
     if (computerSelection == 1) {
-        computerSelection = "Rock";
+        computerSelection = "rock";
     } else if (computerSelection == 2) {
-        computerSelection = "Paper";
+        computerSelection = "paper";
     } else {
-        computerSelection = "Scissors";
+        computerSelection = "scissors";
     }
 }
 
@@ -23,3 +23,23 @@ function userPlay() {
 
 userPlay();
 console.log(userSelection);
+
+function declareWinner () {
+    if (userSelection == computerSelection) {
+        alert("It was a tie! You both played the same hand.");
+    } else if (userSelection == "rock" && computerSelection == "paper") {
+        alert("You lost! Paper beats rock.");
+    } else if (userSelection == "rock" && computerSelection == "scissors") {
+        alert("You won! Rock beats scissors.");
+    } else if (userSelection == "paper" && computerSelection == "rock") {
+        alert("You won! Paper beats rock.");
+    } else if (userSelection == "paper" && computerSelection == "scissors") {
+        alert("You lost! Scissors beats paper.");
+    } else if (userSelection == "scissors" && computerSelection == "rock") {
+        alert("You lost! Rock beats scissors.");
+    } else if (userSelection == "scissors" && computerSelection == "paper") {
+        alert("You won! Scissors beats paper.")
+    }
+}
+
+declareWinner();
